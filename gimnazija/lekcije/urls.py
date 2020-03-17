e54599ca66d662b2066afc5e9ffc7b3432a0691d
Bakir <bakir.feniks@gmail.com>
2020-03-17 19:12:19 +0100

    Napravio nekoliko stranica da rade

--- gimnazija/lekcije/urls.py
+++ gimnazija/lekcije/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='lekcije-home'),
+    path('prvi_razred/', views.prvi_razred, name='prvi-razred'),
+    path('drugi_razred/', views.drugi_razred, name='drugi-razred'),
+    path('trci_razred/', views.treci_razred, name='treci-razred'),
+    path('cetvrti_razred/', views.cetvrti_razred, name='cetvrti-razred'),
+    path('matematika_prvi/', views.matematika_prvi, name='matematika-prvi'),
 ]
--- gimnazija/lekcije/views.py
+++ gimnazija/lekcije/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'lekcije/home.html')
+
+
+def prvi_razred(request):
+    return render(request, 'lekcije/prvi_razred.html')
+
+
+def drugi_razred(request):
+    return render(request, 'lekcije/drugi_razred.html')
+
+
+def treci_razred(request):
+    return render(request, 'lekcije/treci_razred.html')
+
+
+def cetvrti_razred(request):
+    return render(request, 'lekcije/cetvrti_razred.html')
+
+
+def matematika_prvi(request):
+    return render(request, 'lekcije/matematika_prvi.html')
